977138e82c9739e41e7de3d20305dd5516f29820
cilledyr <47023263+cilledyr@users.noreply.github.com>
2020-01-14 16:19:07 +0100

    Added movie things
    Added methods  for updating the movies in the DB, as well as for deleting movies, i also altered the BE to take ID, location and duration into it.

--- src/sqlmoviedatabase/be/Movie.java
+++ src/sqlmoviedatabase/be/Movie.java
@@ -11,45 +11,73 @@ package sqlmoviedatabase.be;
  * @author filip
  */
 public class Movie {
-private String Title;
-private String Category;
-private int Personal_rating;
-private float Imdb_rating;
-private String Lastview;
-public Movie(String title,int personal_rating,float imdb_rating,String lastview ,String category)
-{
-  this.Title = title;
-  this.Category = category;
-  this.Personal_rating = personal_rating;
-  this.Imdb_rating = imdb_rating;
-  this.Lastview = lastview;
-}
-
-public String getTitle()
-{
-    return Title;
-}
-public String getCategory()
-{
-    return Category;
-}
-
-public void setCategory(String cat)
-{
-    Category = cat;
-}
+    private int MovId;
+    private String Title;
+    private String Category;
+    private int Personal_rating;
+    private float Imdb_rating;
+    private String Lastview;
+    private String Duration;
+    private String FileLocation;
     
-public int getPersonal_Rating()
-{
-    return Personal_rating;
-}
-public float getImdb_Rating()
-{
-    return Imdb_rating;
-}
-public String getLastView()
-{
-    return Lastview;
-}
+    public Movie(int movid, String title,int personal_rating,float imdb_rating, String lastview ,String fileLocation, String category, String duration)
+    {
+        this.MovId = movid;
+        this.Title = title;
+        this.Category = category;
+        this.FileLocation = fileLocation;
+        this.Personal_rating = personal_rating;
+        this.Imdb_rating = imdb_rating;
+        this.Lastview = lastview;
+        this.Duration = duration;
+    }
+    
+    public int getmovId()
+    {
+        return MovId;
+    }
+
+    public String getFileLocation()
+    {
+        return FileLocation;
+    }
+
+    public String getTitle()
+    {
+        return Title;
+    }
+    public String getCategory()
+    {
+        return Category;
+    }
+
+    public void setCategory(String cat)
+    {
+        Category = cat;
+    }
+
+    public int getPersonal_Rating()
+    {
+        return Personal_rating;
+    }
+    public float getImdb_Rating()
+    {
+        return Imdb_rating;
+    }
+    public String getLastView()
+    {
+        return Lastview;
+    }
+    
+    public String getDuration()
+    {
+        return Duration;
+    }
+    
+    public void setDuration(String duration)
+    {
+        this.Duration = duration;
+    }
+    
 }
 
--- src/sqlmoviedatabase/dal/MovieDAO.java
+++ src/sqlmoviedatabase/dal/MovieDAO.java
@@ -14,6 +14,7 @@ package sqlmoviedatabase.dal;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -39,24 +40,28 @@ public class MovieDAO {
 
         List<Movie> movies = new ArrayList();//create a list to store all our movies
         try (Connection con = ds.getConnection()){
-            String sqlStatement = "SELECT M.Title, M.Personal_Rating, M.IMDB_Rating, M.LastView, M.FileLink, C.CatName FROM Movies M, Categories C, CatMov  CM WHERE M.MovId = CM.MovId AND CM.CatId=C.CatId ORDER BY M.Title;"; //The SQL statement
+            String sqlStatement = "SELECT M.MovId, M.Title, M.Personal_Rating, M.IMDB_Rating, M.LastView, M.FileLink, M.Duration, C.CatName FROM Movies M, Categories C, CatMov  CM WHERE M.MovId = CM.MovId AND CM.CatId=C.CatId ORDER BY M.Title;"; //The SQL statement
             Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(sqlStatement);//fetch the result of the SQL statement and store it.
             while(rs.next())//While there is something listed...
             {
                 //Fetch all the relevant info
+                int movid = rs.getInt("MovId");
                 String title = rs.getString("Title");
                 String category = rs.getString("CatName") + " ";
                 int personalrating = rs.getInt("Personal_Rating");
                 float imdbrating = rs.getFloat("IMDB_Rating");
                 String lastview = rs.getString("LastView");
-                
+                String filelocation =rs.getString("FileLink");
+                String duration =rs.getString("Duration");
+
                 
 
                 
+                
                 if(movies.isEmpty()){
                     //Create a new instance of the busioness entity Song with the values.
-                    Movie p = new Movie(title,personalrating,imdbrating,lastview,category);
+                    Movie p = new Movie(movid, title,personalrating,imdbrating,lastview,filelocation,category,duration);
                     movies.add(p);//Add the movie to the list.
                 }
                 
@@ -78,7 +83,7 @@ public class MovieDAO {
                 
                     if(movieedit == false){
                         //Create a new instance of the busioness entity Song with the values.
-                        Movie p = new Movie(title,personalrating,imdbrating,lastview,category);
+                        Movie p = new Movie(movid, title,personalrating,imdbrating,lastview,filelocation,category,duration);
                         movies.add(p);//Add the movie to the list.
                     }
                 
@@ -91,4 +96,69 @@ public class MovieDAO {
         
         return movies;//return the list of movies.
     }
+    
+    public Movie UpdateMovie (Movie movie, String title, int personalRating, float imdbRating, String lastView, String fileLocation, String duration, String categories){
+        try(Connection con = ds.getConnection()){
+        
+            String sqlUpdateMovie = "UPDATE Movies SET Personal_Rating=?, IMDB_Rating=?, LastView=?, FileLink=?, Duration=? WHERE Title=?;";
+            PreparedStatement pstmt = con.prepareStatement(sqlUpdateMovie);
+            
+            pstmt.setInt(1, personalRating);
+            pstmt.setFloat(2,imdbRating);
+            pstmt.setString(3, "" + lastView + "");
+            pstmt.setString(4, "" + fileLocation + "");
+            pstmt.setString(5, "" + duration + "");
+            pstmt.setString(5, "" + title+ "");
+            pstmt.execute();
+            
+            String sqlRemoveCategories = "DELETE FROM CatMov WHERE MovID=?";
+            PreparedStatement remstmt =con.prepareStatement(sqlRemoveCategories);
+            remstmt.setInt(1, movie.getmovId());
+            remstmt.execute();
+            
+            String[] cat = categories.split(",");
+            
+            for(int i=0; i<cat.length ; i++){
+                String catstmt = "SELECT * FROM Categories WHERE CatName=?;";
+                Statement statement = con.createStatement();
+                ResultSet crs = statement.executeQuery(catstmt);//fetch the result of the SQL statement and store it.
+                while(crs.next()){ //While there is something listed...
+                    int categoryID = crs.getInt("CatId");
+                    String enterCat ="INSERT INTO CatMov VALUES MovID=?, CatID=?";
+                    PreparedStatement catSetstmt = con.prepareStatement(enterCat);
+                    catSetstmt.setInt(1, movie.getmovId());
+                    catSetstmt.setInt(2, categoryID);
+                    catSetstmt.execute();
+                }
+            }
+            
+            
+        } catch (SQLServerException ex) {
+            Logger.getLogger(MovieDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Unable to update Movie.");
+        } catch (SQLException ex) {
+            Logger.getLogger(MovieDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Unable to update Movie.");
+        }
+        
+        return new Movie(movie.getmovId(), title, personalRating, imdbRating, lastView, fileLocation, categories, duration);
+    }
+
+    public void deleteMovie(Movie movie){
+        try(Connection con = ds.getConnection()){
+            String sqldeletestmt = "DELETE FROM CatMov WHERE MovId=? ; DELETE FROM Movies WHERE MovId=?;";
+            PreparedStatement delstmt = con.prepareStatement(sqldeletestmt);
+            delstmt.setInt(1, movie.getmovId());
+            delstmt.setInt(2, movie.getmovId());
+            delstmt.execute();
+            
+        } catch (SQLServerException ex) {
+            Logger.getLogger(MovieDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Unable to delete Movie.");
+        } catch (SQLException ex) {
+            Logger.getLogger(MovieDAO.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Unable to delete Movie.");
+        }
+    }
+
 }
